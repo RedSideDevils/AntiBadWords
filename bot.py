11fb6b0cc8618bd0eaab4701a1d23b244a466654
YanOScompany <56045160+YanOScompany@users.noreply.github.com>
2020-10-19 02:47:39 +0400

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,33 @@
+import discord 
+from discord.ext import commands
+
+PREFIX = '.'
+
+client = commands.Bot(command_prefix = PREFIX )
+
+#words
+bad_words = ['suchka','blyat','blya','suka','pizdec','blat','emae','idi naxuy','nax','pidaras','ebal', 'eblan','ebanutiy','fuck','fuck off','shit','ueban','shut up','ass','pussy','hole','gandon','zaebis','zaebal','asshole']
+
+@client.event 
+
+async def on_ready():
+    print('[~]Bot By Trippingcarpet[~]')
+    print('Version: 1.0.0')
+
+@client.event
+
+async def on_message(message):
+    msg = message.content.lower().split()
+
+    try:
+
+        for n in msg:
+            if n in bad_words:        
+                await message.delete()
+                await message.channel.send('BAD WORD DETECTED {0.author.mention}'.format(message))
+
+    except:
+        pass
+
+
+client.run("NzY3NDg0NjI5ODgzMjg5NjEx.X4ylwg.sc4GUKXt2Onzn1L1-jK_uEnY8Rw")
